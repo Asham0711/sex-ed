b33dfe5f9f8e586acaf9c564d16ccb019e23b94e
Asham0711 <mdashamimad3246@gmail.com>
2024-09-27 19:13:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center bg-red-600">
+        Hello Motherfuckers
+      </div>
+    </>
+  )
+}
+
+export default App
